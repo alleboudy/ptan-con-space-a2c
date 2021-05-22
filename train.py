75f441da535824adbd4d75f28b67d52c91f60a6d
ronin <ahmad.alleboudy@outlook.com>
2021-05-22 11:39:22 +0200

    initial commit -> added simple a2c pipeline by ptan

---
+++ train.py
@@ -0,0 +1,249 @@
+import argparse
+import gym
+import pybullet_envs  # this is a must although we don't explicitly use it!
+import os
+from tensorboardX import SummaryWriter
+from torch import optim
+import time
+
+import ptan
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+HID_SIZE = 128
+ENV_ID = "MinitaurBulletEnv-v0"
+GAMMA = 0.99
+REWARD_STEPS = 2
+BATCH_SIZE = 32
+LEARNING_RATE = 5e-5
+ENTROPY_BETA = 1e-4
+TEST_ITERS = 1000
+RENDER = False
+# the model itself -> 3 heads, one for mu, one for var and one for the value
+
+
+class ModelA2C(nn.Module):
+    def __init__(self, obs_size, act_size):
+        super(ModelA2C, self).__init__()
+
+        self.base = nn.Sequential(
+            nn.Linear(obs_size, HID_SIZE),
+            nn.ReLU(),
+        )
+        self.mu = nn.Sequential(
+            nn.Linear(HID_SIZE, act_size),
+            nn.Tanh(),
+        )
+        self.var = nn.Sequential(
+            nn.Linear(HID_SIZE, act_size),
+            nn.Softplus(),  # (log(1+e**x)) -> a smoothed relu for a positive variance
+        )
+        self.value = nn.Linear(HID_SIZE, 1)
+
+    def forward(self, x):
+        base_out = self.base(x)
+        return self.mu(base_out), self.var(base_out), \
+            self.value(base_out)
+
+
+# a PTAN agent that uses the network to convert observations into actions
+class AgentA2C(ptan.agent.BaseAgent):
+    def __init__(self, net, device="cpu"):
+        self.net = net
+        self.device = device
+
+    def __call__(self, states, agent_states):
+        states_v = ptan.agent.float32_preprocessor(states)
+        states_v = states_v.to(self.device)
+        mu_v, var_v, _ = self.net(states_v)
+        mu = mu_v.data.cpu().numpy()
+        sigma = torch.sqrt(var_v).data.cpu().numpy()
+        actions = np.random.normal(mu, sigma)
+        actions = np.clip(actions, -1, 1)
+        # not liking the clipping
+        # https://stackoverflow.com/questions/18441779/how-to-specify-upper-and-lower-limits-when-using-numpy-random-normal
+        return actions, agent_states
+
+
+def test_net(net, env, count=10, device="cpu"):
+    rewards = 0.0
+    steps = 0
+    for _ in range(count):
+        obs = env.reset()
+        while True:
+            obs_v = ptan.agent.float32_preprocessor([obs])
+            obs_v = obs_v.to(device)
+            mu_v = net(obs_v)[0]
+            action = mu_v.squeeze(dim=0).data.cpu().numpy()
+            action = np.clip(action, -1, 1)
+            obs, reward, done, _ = env.step(action)
+            rewards += reward
+            steps += 1
+            if done:
+                break
+    return rewards / count, steps / count
+
+
+def calc_logprob(mu_v, var_v, actions_v):
+    p1 = -((mu_v - actions_v) ** 2) / (2*var_v.clamp(min=1e-3))
+    p2 = -torch.log(torch.sqrt(2 * math.pi * var_v))
+    return p1 + p2
+
+
+def unpack_batch_a2c(batch, net, last_val_gamma, device="cpu"):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(exp.state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(exp.last_state)
+    # the redundant np.array() inside the ptan.agent.float32_preprocessor is due to the issue 13918
+    states_v = ptan.agent.float32_preprocessor(states).to(device)
+    actions_v = torch.FloatTensor(actions).to(device)
+    rewards_np = np.array(rewards, dtype=np.float32)
+    # note that, the V(s) term is (Sum_{i-N} GAMMA^i x r_i) + GAMMA^N V(S_N) -> (bellman q for N steps)
+    # and the rewards we got rewards_np are just the first part before the +
+    # because the ExperineceSourceFirstLast returns rewards as it already discounted for the subtrajectory
+    # so, we are missing the second part, only for the not done steps
+    # this is the opposite of before with DQN where we just masked Q'(s_{t+1},a) for done episodes, because we had Q values as nw output
+    # here we have advantages instead
+    # handle rewards
+
+    if not_done_idx:
+        last_states_v = ptan.agent.float32_preprocessor(last_states).to(device)
+        last_vals_v = net(last_states_v)[2]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += last_val_gamma * last_vals_np
+
+    # NOTE: ref_vals_v is not actually the q or the v, but the TD(N) return
+    # so, it is not also the return G(t), but just till N steps
+    ref_vals_v = torch.FloatTensor(rewards_np).to(device)
+    return states_v, actions_v, ref_vals_v
+
+
+def unpack_batch_ddqn(batch, device="cpu"):
+    states, actions, rewards, dones, last_states = [], [], [], [], []
+    for exp in batch:
+        states.append(exp.state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        dones.append(exp.last_state is None)
+        if exp.last_state is None:
+            last_states.append(exp.state)
+        else:
+            last_states.append(exp.last_state)
+    states_v = ptan.agent.float32_preprocessor(states).to(device)
+    actions_v = ptan.agent.float32_preprocessor(actions).to(device)
+    rewards_v = ptan.agent.float32_preprocessor(rewards).to(device)
+    last_states_v = ptan.agent.float32_preprocessor(last_states).to(device)
+    dones_t = torch.BoolTensor(dones).to(device)
+    return states_v, actions_v, rewards_v, dones_t, last_states_v
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False,
+                        action='store_true', help='Enable CUDA')
+    parser.add_argument("-n", "--name", required=True, help="Name of the run")
+    args = parser.parse_args()
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    save_path = os.path.join("saves", "a2c-" + args.name)
+    os.makedirs(save_path, exist_ok=True)
+    spec = gym.envs.registry.spec(ENV_ID)
+    spec._kwargs['render'] = RENDER
+
+    env = gym.make(ENV_ID)
+    test_env = gym.make(ENV_ID)
+
+    net = ModelA2C(
+        env.observation_space.shape[0], env.action_space.shape[0]).to(device)
+    print(net)
+
+    writer = SummaryWriter(comment="-a2c_" + args.name)
+    agent = AgentA2C(net, device=device)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(
+        env, agent, GAMMA, steps_count=REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    batch = []
+    best_reward = None
+    with ptan.common.utils.RewardTracker(writer) as tracker:
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            for step_idx, exp in enumerate(exp_source):
+                rewards_steps = exp_source.pop_rewards_steps()
+                if rewards_steps:
+                    rewards, steps = zip(*rewards_steps)
+                    tb_tracker.track("episode_steps", steps[0], step_idx)
+                    tracker.reward(rewards[0], step_idx)
+
+                if step_idx % TEST_ITERS == 0:
+                    ts = time.time()
+                    rewards, steps = test_net(net, test_env, device=device)
+                    print("Test done is %.2f sec, reward %.3f, steps %d" % (
+                        time.time() - ts, rewards, steps))
+                    writer.add_scalar("test_reward", rewards, step_idx)
+                    writer.add_scalar("test_steps", steps, step_idx)
+                    if best_reward is None or best_reward < rewards:
+                        if best_reward is not None:
+                            print("Best reward updated: %.3f -> %.3f" %
+                                  (best_reward, rewards))
+                            name = "best_%+.3f_%d.dat" % (rewards, step_idx)
+                            fname = os.path.join(save_path, name)
+                            torch.save(net.state_dict(), fname)
+                        best_reward = rewards
+
+                batch.append(exp)
+                if len(batch) < BATCH_SIZE:
+                    continue
+
+                states_v, actions_v, vals_ref_v = \
+                    unpack_batch_a2c(
+                        batch, net, device=device,
+                        last_val_gamma=GAMMA ** REWARD_STEPS)
+                batch.clear()
+
+                optimizer.zero_grad()
+                mu_v, var_v, value_v = net(states_v)
+
+                loss_value_v = F.mse_loss(
+                    value_v.squeeze(-1), vals_ref_v)
+
+                adv_v = vals_ref_v.unsqueeze(dim=-1) - \
+                    value_v.detach()
+                log_prob_v = adv_v * calc_logprob(
+                    mu_v, var_v, actions_v)
+                loss_policy_v = -log_prob_v.mean()
+                ent_v = -(torch.log(2*math.pi*var_v) + 1)/2
+                entropy_loss_v = ENTROPY_BETA * ent_v.mean()
+
+                loss_v = loss_policy_v + entropy_loss_v + \
+                    loss_value_v
+                loss_v.backward()
+                optimizer.step()
+
+                tb_tracker.track("advantage", adv_v, step_idx)
+                tb_tracker.track("values", value_v, step_idx)
+                tb_tracker.track("batch_rewards", vals_ref_v, step_idx)
+                tb_tracker.track("loss_entropy", entropy_loss_v, step_idx)
+                tb_tracker.track("loss_policy", loss_policy_v, step_idx)
+                tb_tracker.track("loss_value", loss_value_v, step_idx)
+                tb_tracker.track("loss_total", loss_v, step_idx)
